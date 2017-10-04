b35d7ec4ef646331068988b96cec9f86f24c8d4c
schreibersebastian.de <breakpoint@schreibersebastian.de>
2017-10-04 22:02:50 +0200

    [BUGFIX] Change version constraint for TYPO3

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.6.99',
+            'typo3' => '6.2.0-8.7.99',
         ],
         'conflicts' => [
         ],
